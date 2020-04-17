75e75cefbad7f9938c8e5d5c95008f371c9e5be5
Ligia Abril <labrild@miumg.edu.gt>
2020-04-17 13:39:25 -0600

    Agregar area para conexion a mase de datos

---
+++ RentaVideos/RentaVideos/Conexion/Conexion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.Odbc;
+
+namespace RentaVideos.Conexion
+{
+    class Conexion
+    {
+        OdbcConnection conn;
+        public Tuple<OdbcConnection, OdbcTransaction> conexion()
+        {
+            conn = new OdbcConnection("Dsn=rentavideodb");// creacion de la conexion via ODBC
+            OdbcTransaction transaccion = null;
+            try
+            {
+                conn.Open();
+                transaccion = conn.BeginTransaction();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+
+            return Tuple.Create(conn, transaccion);
+        }
+
+        public void desconexion()
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+
+        }
+    }
+}
